ca180f544e2358bd1cb49a8c4760ff447eac0c9c
Thomas Slock <tslock@gmail.com>
2017-02-01 10:13:47 +0100

    App doesn't need to extends component

--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 
@@ -6,18 +6,16 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import AddForm from '../components/AddForm';
 
-export class App extends Component {
-    render(){
-        return (
-            <div id="app">
-                <Header user={this.props.user} />
-                {this.props.page}
-                <AddForm />
-                <Footer />
-            </div>
-        )
-    }
-}
+const App = (props) =>  {
+    return (
+        <div id="app">
+            <Header user={props.user} />
+            {props.page}
+            <AddForm />
+            <Footer />
+        </div>
+    )
+};
 
 App.propTypes = {
     user: PropTypes.object
